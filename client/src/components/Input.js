4d6acd19ae8a19e0b0d17d6189cbd4f8cada292c
tkhwang <tkhwang@gmail.com>
2018-12-05 10:01:43 +0900

    [+] Local : Google Translation API usage.

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import "./Input.css";
 
 class Input extends React.Component {
   constructor(props) {
